41fddd5b41d332c925d6c9186edf60a4e4d39aae
Айси <IceIX@MacBook-Pro-Filipp.local>
2022-10-18 13:22:02 +0300

    zadacha1

---
+++ задача1/Program.cs
@@ -0,0 +1,35 @@
+﻿/* напишите программу, котора выводит cлучанйное число из отрезка (10, 99) показывает наибольшую цифру числа.*/
+//метод нахождения наибольшей цифры числа
+//получение радномного числа от minValue до maxValue
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int GetNumber(int minValue, int maxValue)
+        {
+            Random rdn = new Random();
+            int result = rdn.Next(minValue, maxValue);
+            return result;
+
+            Console.WriteLine();
+        }
+
+//метод нахождения наибольшей цифры числа
+        int GetMaxDigitOfNumber(int number)
+        {
+            int firstDigit = number / 10;
+            int secondDigit = number % 10;
+
+            if (firstDigit > secondDigit)
+                return firstDigit;
+            else
+                return secondDigit;
+        }
+
+        int number = GetNumber(10, 100);
+        int maxDigit = GetMaxDigitOfNumber(number);
+
+        Console.WriteLine($"Максмальное число: {number} Максимальная цифра:{maxDigit}");
+    }
+}
