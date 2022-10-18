38e9be3336a75020b8d581085ed6ad2ec048f600
Айси <IceIX@MacBook-Pro-Filipp.local>
2022-10-18 13:22:31 +0300

    zadacha2

---
+++ задача2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+void AnswerToAsk(){
+
+int number = (new Random()).Next(100, 1000);
+Console.WriteLine($"рандомное число: {number}");
+string numericAsString = number.ToString();
+int result = int.Parse(numericAsString[0].ToString()+numericAsString[].ToString());
+Console.WriteLine($"рандомное число без 2ой цифры: {number}");
+}
+
+AnswerToAsk();
